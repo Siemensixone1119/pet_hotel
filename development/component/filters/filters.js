160a4c2de0f8f02e3d80e6e20f2a0bf479d49970
Alexandra Baranova <siemensixone1119@gmail.com>
2025-02-26 22:49:24 +0300

    add adaptive style rooms pages

--- development/component/filters/filters.js
+++ development/component/filters/filters.js
@@ -1,7 +1,50 @@
 import filtersHTML from "../filters/filters.html";
 
 document.addEventListener("DOMContentLoaded", function () {
-    const filtersElement = document.getElementById("filters");
-    if (!filtersElement) return;
-    filtersElement.innerHTML = filtersHTML;
+  const filtersElement = document.getElementById("filters");
+  if (!filtersElement) return;
+  filtersElement.innerHTML = filtersHTML;
+
+  // Следим за изменениями в DOM и запускаем initFilter(), когда кнопки появятся
+  const observer = new MutationObserver(() => {
+    if (document.querySelector("[data-open-filter]")) {
+      initFilter();
+      observer.disconnect(); // Отключаем наблюдение после инициализации
+    }
+  });
+
+  observer.observe(document.body, { childList: true, subtree: true });
+  console.log(window.innerWidth);
 });
+
+function initFilter() {
+  const refs = {
+    openFilterBtn: document.querySelector("[data-open-filter]"),
+    closeFilterBtn: document.querySelector("[data-close-filter]"),
+    filter: document.querySelector("[data-filter]"),
+    backdrop: document.querySelector("[data-backdrop]"),
+  };
+  prHidden(refs)
+  refs.openFilterBtn.addEventListener("click", () => toggleFilter(refs));
+  refs.closeFilterBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    toggleFilter(refs);
+  });
+  window.addEventListener("resize", () => prHidden(refs));
+}
+
+function toggleFilter(refs) {
+  refs.filter.classList.toggle("hidden");
+  refs.backdrop.classList.toggle("hidden");
+}
+
+function prHidden(refs) {
+  if (window.innerWidth >= 1024) {
+    refs.filter.classList.remove("hidden");
+  } else {
+    if (!refs.filter.classList.contains("hidden")) {
+      refs.filter.classList.add("hidden");
+      refs.backdrop.classList.add("hidden");
+    }
+  }
+}
